86fd8a38647c4a9834056100e6a253cefabbb44c
micahwierenga <wierenga.micah@gmail.com>
2017-07-16 20:51:08 -0600

    Added process.env.PORT variable to server.js to try to fix heroku deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use( function( req, res ) {
 	res.sendFile( __dirname + '/public/index.html' );
 });
 
-app.listen( 3000, function() {
+app.listen( process.env.PORT || 3000, function() {
 	console.log( 'Bless Thee App listening on localhost:3000' );
 })
 
